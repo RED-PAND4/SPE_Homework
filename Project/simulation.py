ad69ad79b5b7a818751ba42fec7f5f342f097cff
davipase <davipase@gmail.com>
2025-01-20 10:44:03 +0100

    added simulation file

---
+++ Project/simulation.py
@@ -0,0 +1,36 @@
+from graph import Bianconi_Barabasi_network
+from scipy.stats import uniform
+from scipy.stats import expon
+from scipy.stats import alpha
+from scipy.stats import arcsine
+from scipy.stats import rv_discrete
+
+
+#creates a simulation of the model printing the final network.
+# m: the number of connection a node will make
+# n: the number of new nodes to be added
+# distribution: the distribution used to generate the fitnesses
+def run_printed_simulation(m, n, distribution):
+    nw = Bianconi_Barabasi_network(m,distribution)
+    for _ in range(0,n,1):
+        nw.add_node() 
+    nw.print_fitnesses()
+    nw.plot()
+
+#creates a simulation of the model without printing the final network.
+# m: the number of connection a node will make
+# n: the number of new nodes to be added
+# distribution: the distribution used to generate the fitnesses
+# top: the number of nodes with the highest fitness to be printed
+def run_printless_simulation(m, n, distribution, top):
+    nw = Bianconi_Barabasi_network(m,distribution)
+    for _ in range(0,n,1):
+        nw.add_node() 
+    nw.print_top(top)
+
+
+constant = rv_discrete(name='constant', values=([1], [1.0]))
+run_printed_simulation(7,3, uniform)
+# run_printless_simulation(7,1, uniform, 50)
+
+
